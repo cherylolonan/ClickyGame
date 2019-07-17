3931d1a88f9b8a2f71de3e50bae8c86c4e96103a
shrill <cheryl.olonan@gmail.com>
2019-07-16 20:03:44 -0700

    Updates

---
+++ precache-manifest.911f8045fbbd7bb7c609dab1f44f055f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c987f7eb4a2da919b6a2822abb1652b4",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "d7d2e708e0dca81424b9",
+    "url": "/ClickyGame/static/css/main.e5d44acb.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "d7d2e708e0dca81424b9",
+    "url": "/ClickyGame/static/js/main.5ac95f17.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
